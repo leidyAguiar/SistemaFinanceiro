b20537d244f4c7d3c43916cef36d934ef95fe95d
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2022-11-12 18:04:46 -0300

    Criou register e config, moficações na tela login

--- register.php
+++ register.php
@@ -10,11 +10,11 @@ require_once("config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo $nomeSistema ?> </title>
+    <title><?php echo $nomeSistema ?></title>
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!--CSS-->
-    <link href="./css/style.css" rel="stylesheet" >
+    <link href="./css/style.css" rel="stylesheet">
     <!--Icon-->
     <link href="./img/dolar.png" rel="shortcut icon" type= "image/x-icon">
 </head>
@@ -23,10 +23,10 @@ require_once("config.php");
     <div class="container">
         <div class="row content">
             <div class="col-md-6 mb-3">
-                <img src="./img/PageLogin.png" class="img-fluid" alt="image">
+                    <img src="./img/PageRegister.png" class="img-fluid" alt="image">
             </div>
             <div class="col-md-6">
-                <h3 class="signin-text mb-3"> Login </h3>
+                <h3 class="signin-text mb-3"> Criar Conta</h3>
                 <br>
                 <form method="post">
                     <div>
@@ -39,18 +39,18 @@ require_once("config.php");
                         <input type="password" name="senha" class="form-control" id="senha">
                     </div>
                     <br></br>
-                    <div class="form-group from-check">
-                        <input type="checkbox" name="checkbox" class="form-check-input" id="checkbox">
-                        <label class="form-check-label" for="checkbox">Lembrar-me</label>
+                    <div>
+                        <label for="senha">Confirme sua senha</label>
+                        <input type="password" name="senha" class="form-control" id="senha">
                     </div>
                     <br></br>
-                    <button class="btn btn-class">Entrar</button>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Enviar">
+                        <input type="reset" class="btn btn-secondary" value="Limpar">
+                    </div>
                     <br></br>
-                    <p>Não possui uma conta? <a href="register.php">Criar conta</a></p>
+                    <p>Possui uma conta? <a href="login.php">Fazer login</a></p>
                 </form>
-            </div>
-        </div>
-    </div>
 </body>
 
 </html>
