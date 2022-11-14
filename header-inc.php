047ef03ed43b74140a8e9188aa63ab83353086cf
evandroribeirogomes <evribeirgo_07@hotmail.com>
2022-11-14 00:23:02 -0300

    Update header-inc.php

--- header-inc.php
+++ header-inc.php
@@ -13,6 +13,17 @@ session_start();
     <title>NuAzul</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <a class="navbar-brand" href="#"><h2 style="color:white;">NuAzul</h2></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      
+        <a class="nav-link" href="login.php"><h2>Login</h2></a>
+            
+    </div>
+</nav>
 
 </body>
 </html>
