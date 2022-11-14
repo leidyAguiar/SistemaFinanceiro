0573ac83847299aa999c08411984915322a9d79a
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2022-11-14 19:26:06 -0300

    Alterações index, alteração css

---
+++ index2.php
@@ -0,0 +1,46 @@
+<?php
+require_once("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title> <?php echo $nomeSistema ?> </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+    <!--CSS-->
+     <link rel="stylesheet" type="text/css" href="./css/style.index2.css">
+     <!--Icon-->
+     <link href="./img/dolar.png" rel="shortcut icon" type= "image/x-icon">
+</head>
+
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><img src="./img/logoBranca3.png" alt=""></label>
+        <ul>
+            <li><a href="./login.php">Login</a></li>
+        </ul>
+    </nav>
+    <main>
+        <aside>
+            <h2>Sejam bem-vindos e conheçam NuAzul</h2>
+            <p>NuAzul foi desenvolvida para você, que deseja dar um Up em sua vida financeira, organizando suas contas com um sistema que atende as suas necessidades</p>
+        </aside>
+    </main>
+
+    <article> <!--div p imagem-->
+        <img src="./img/home.png" alt="">
+    </article>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
