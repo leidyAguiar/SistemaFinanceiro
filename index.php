c18793c24a0cf44893f5ebc4d7b0321906ce3984
evandroribeirogomes <evribeirgo_07@hotmail.com>
2022-11-14 18:09:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style-index.css">
-    <title>Document</title>
+    <title>NuAzul</title>
 </head>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 
