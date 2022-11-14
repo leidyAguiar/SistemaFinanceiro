00ec7644d92129c8a1cb55fdedc0ec682658f630
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2022-11-14 20:15:35 -0300

    delete pages e update

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
+<?php
+require_once("config.php");
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <title> <?php echo $nomeSistema ?> </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style-index.css">
-    <title>NuAzul</title>
+     
+    <!--CSS-->
+     <link rel="stylesheet" type="text/css" href="./css/style.index.css">
+     <!--Icon-->
+     <link href="./img/dolar.png" rel="shortcut icon" type= "image/x-icon">
 </head>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
 
 <body>
-<?php
-     require("header-inc.php"); 
-?>
-<div>
-    <figure>
-    <img src="img/logo.png" class="img-fluid" alt="Responsive image">
-    </figure>
-</div>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo"><img src="./img/logoBranca3.png" alt=""></label>
+        <ul>
+            <li><a href="./login.php">Login</a></li>
+        </ul>
+    </nav>
+    <main>
+        <aside>
+            <h2>Sejam bem-vindos e conheçam NuAzul</h2>
+            <p>NuAzul foi desenvolvida para você, que deseja dar um Up em sua vida financeira, organizando suas contas com um sistema que atende as suas necessidades</p>
+        </aside>
+    </main>
+
+    <article> <!--div p imagem-->
+        <img src="./img/home.png" alt="">
+    </article>
+            </div>
+        </div>
+    </div>
+
 </body>
-<?php require("footer.inc.php"); ?>
+
 </html>
