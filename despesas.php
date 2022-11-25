39d31a14024026f21227ef262af3ffd2a5948259
Yasmin Gabrielle <102554669+YasminGab@users.noreply.github.com>
2022-11-25 16:47:32 -0300

    Correção do Merge

--- despesas.php
+++ despesas.php
@@ -44,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 $result = $conn->query($mysql_query);
 
+//Connection Close
 mysqli_close($conn);
 ?>
 
@@ -63,14 +64,19 @@ mysqli_close($conn);
   <link href="./css/style-dashboard.css" rel="stylesheet">
   <!--Icon-->
   <link href="./img/dolar.png" rel="shortcut icon" type="image/x-icon">
+
+
   <!--Icones da tela-->
   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+
+
+
 </head>
 
 <body>
   <?php require("menu_lateral.php"); ?>
   <div class="container">
-  <div class="row content">
+    <div class="row content">
       <h2 class="signin-text mb-3">Despesas</h2>
       <p>Listagem do despesas cadastradas.</p>
       <hr>
@@ -114,34 +120,34 @@ mysqli_close($conn);
           </div>
           </br>
         </form>
-      <a href="./insert_despesa.php"><button type="button" class="btn btn-primary">+ Novo</button></a>
-    </div>
-    <table class="table table-striped table-bordered table-hover">
+        <a href="./insert_despesa.php"><button type="button" class="btn btn-primary-acao">+ Novo</button></a>
+      </div>
+      <table class="table table-striped table-bordered table-hover">
 
-      <thead>
-        <tr class="table-info" style="text-align:center">
-          <th scope="col" style="width: 5%;">#</th>
-          <th scope="col">Descrição</th>
-          <th scope="col" style="width: 20%;">Data Transação</th>
-          <th scope="col" style="width: 15%;">Valor Transação</th>
-          <th scope="col" style="width: 20%;">Ação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php while ($row = $result->fetch_assoc()) { ?>
-          <tr>
-            <td style="text-align:center"><?php echo $row['tran_id']; ?></td>
-            <td style="text-align:center"><?php echo $row['tran_descricao']; ?></td>
-            <td style="text-align:center"><?php echo (new DateTime($row['tran_data']))->format('Y-m-d'); ?></td>
-            <td style="text-align:center"><?php echo $row['tran_valor']; ?></td>
-            <td style="display:flex;">
-              <a href="edit_despesa.php?tran_id=<?php echo $row['tran_id']; ?>"><button type="button" class="btn btn-primary">Editar</button></a>
-              <a href="delete_despesa.php?tran_id=<?php echo $row['tran_id']; ?>"><button type="button" class="btn btn-danger">Excluir</button></a>
-            </td>
+        <thead>
+          <tr class="table-info" style="text-align:center">
+            <th scope="col" style="width: 5%;">#</th>
+            <th scope="col">Descrição</th>
+            <th scope="col" style="width: 20%;">Data Transação</th>
+            <th scope="col" style="width: 15%;">Valor Transação</th>
+            <th scope="col" style="width: 20%;">Ação</th>
           </tr>
-        <?php } ?>
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody>
+          <?php while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+              <td style="text-align:center"><?php echo $row['tran_id']; ?></td>
+              <td style="text-align:center"><?php echo $row['tran_descricao']; ?></td>
+              <td style="text-align:center"><?php echo (new DateTime($row['tran_data']))->format('Y-m-d'); ?></td>
+              <td style="text-align:center"><?php echo $row['tran_valor']; ?></td>
+              <td style="display:flex;">
+                <a href="edit_despesa.php?tran_id=<?php echo $row['tran_id']; ?>"><button type="button" class="btn btn-primary-acao">Editar</button></a>
+                <a href="delete_despesa.php?tran_id=<?php echo $row['tran_id']; ?>"><button type="button" class="btn btn-danger">Excluir</button></a>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
 
 </body>
