9db8f2d63e65fab2b8d473bd67866e808a965677
Evandro Ribeiro <evribeirgo_07@hotmail.com>
2022-11-26 09:51:59 -0300

    Update insert_receita.php

--- insert_receita.php
+++ insert_receita.php
@@ -1,13 +1,11 @@
 <?php
+
 session_start();
-
 require_once("enum.php");
-
 require_once("connection.php");
-
 require_once("config.php");
 
-// Check if the user is logged in, if not then redirect him to login page
+
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 	header("location: login.php");
 	exit;
@@ -27,13 +25,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$tran_descricao = $_POST['tran_descricao'];
 		$uso_id = $_SESSION['uso_id'];
 
-		// Mysql query to insert record into table
-
 		$mysql_query = "INSERT INTO transacao (uso_id, tran_data, tran_valor, tran_descricao, tipo_id) VALUES ('{$uso_id}', '{$tran_data}', '{$tran_valor}', '{$tran_descricao}', " . TipoTransacao::RECEITA->value . ")";
 
 		$result = $conn->query($mysql_query);
-
-
 
 		if ($result === TRUE) {
 			$msg =  "insert success";
@@ -43,9 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$msgerror = $conn->error;
 		}
 
-		//Connection Close
 		mysqli_close($conn);
-
 
 		header("Location: receitas.php?msg={$msg}&msgerror={$msgerror}");
 	} else {
@@ -59,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
